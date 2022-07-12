9f29f2916e574ebfce4546c87625079decfea1ce
Kanzoonrkz <kanzoonrkz@gmail.com>
2022-07-12 10:16:38 +0700

    [kanzoon] login register, token stored in cookies

--- layout/loginregisterLayout.js
+++ layout/loginregisterLayout.js
@@ -3,8 +3,10 @@ import React from "react";
 function LoginregisterLayout({ children }) {
   return (
     <section className="row p-0 m-0" style={{ height: "100vh" }}>
-      <div className="col-sm-6 px-0 d-none d-sm-block"
-      style={{position: "relative"}}>
+      <div
+        className="col-6 px-0 d-none d-md-block"
+        style={{ position: "relative" }}
+      >
         <img
           src="/loginregisterbg.png"
           alt="image"
@@ -18,18 +20,25 @@ function LoginregisterLayout({ children }) {
         />
         <div
           className="d-flex flex-column justify-content-center"
-          style={{ height: "100%", color: "white", marginLeft:"10%"}}
+          style={{ height: "100%", color: "white", marginLeft: "10%" }}
         >
-          <h1 className="m-0 p-0" style={{fontSize:"3rem", fontWeight:"700"}}>Second<br />Hand.</h1>
+          <h1
+            className="m-0 p-0"
+            style={{ fontSize: "3rem", fontWeight: "700" }}
+          >
+            Second
+            <br />
+            Hand.
+          </h1>
         </div>
       </div>
 
-      <div className="col-sm-6">
-        <div className="px-3 ms-xl-4 d-lg-none d-xl-none d-md-none mt-5">
+      <div className="col-12 col-md-6">
+        <div className="px-3 ms-xl-4 d-lg-none d-xl-none d-md-none mt-2">
           <i className="bi bi-arrow-left fs-1"></i>
         </div>
 
-        <div className="d-flex flex-column">{children}</div>
+        <div className="mx-auto d-flex flex-column justify-content-center" style={{maxWidth:"600px"}}>{children}</div>
       </div>
     </section>
   );
